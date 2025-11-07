87d2abe1505556d77c7342cb7edddd141c883e88
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-07 16:35:16 +0700

    Inisialisasi Composable dan skema warna

---
+++ app/src/main/java/com/example/questnavigastugas_057/view/HalamanAwal.kt
@@ -0,0 +1,33 @@
+package com.example.questnavigastugas_057.view
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_057.R
+import com.example.questnavigastugas_057.Routes
+
+@Composable
+fun HalamanAwal(navController: NavController) {
+    val backgroundColor = Color(0xFFF3E5F5)
+    val textColor = Color(0xFF3F51B5)
+    val buttonColor = Color(0xFF263A9A)
+
