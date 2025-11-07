efb3000b821e0428fedc9cff98e72eb695d7e00b
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-07 16:25:10 +0700

    Siapkan navigasi antar halaman

---
+++ app/src/main/java/com/example/questnavigastugas_057/Navigasi.kt
@@ -0,0 +1,19 @@
+package com.example.questnavigastugas_057
+
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_057.view.Formulir
+import com.example.questnavigastugas_057.view.HalamanAwal
+import com.example.questnavigastugas_057.view.ListDaftar
+
+
+object Routes {
+    const val HALAMAN_AWAL = "HalamanAwal"
+    const val LIST_DAFTAR = "ListDaftar"
+    const val FORMULIR = "Formulir"
+}
